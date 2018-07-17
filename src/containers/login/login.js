500a9c82e411e26116ab23a9e287baedbba228a4
xu_chuang <mxqhxc@163.com>
2018-07-17 18:54:03 +0800

    xx

--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -1,9 +1,38 @@
 import React,{Component} from "react";
+import {
+  NavBar,
+  WingBlank,
+  List,
+  InputItem,
+  WhiteSpace,
+  Button
+} from 'antd-mobile'
+
+import Logo from "../../components/logo/logo"
 
 class Login extends Component{
+  toRegister = () => {
+    this.props.history.replace("/register")
+  }
   render(){
     return(
-      <h2>Login</h2>
+      <div>
+        <NavBar>登陆</NavBar>
+        <Logo />
+        <WingBlank>
+          <List>
+            <WhiteSpace/>
+            <InputItem placeholder="请输入用户名">用户名: </InputItem>
+            <WhiteSpace/>
+            <InputItem type="password" placeholder="请输入密码">密码: </InputItem>
+            <WhiteSpace/>
+
+            <WhiteSpace/>
+            <Button type="primary">登陆</Button>
+            <Button onClick={this.toRegister}>没有账号</Button>
+          </List>
+        </WingBlank>
+      </div>
     )
   }
 }
--- src/containers/register/register.js
+++ src/containers/register/register.js
@@ -11,7 +11,23 @@ import {
 
 import Logo from "../../components/logo/logo"
 
+
+
+
+
 class Register extends Component{
+  state = {
+    username: '',
+    password: '',
+    password2: '',
+    type: 'dashen'
+  }
+  handlerChange = (name,value) => {
+    this.setState({[name]:value})
+  }
+  toLogin = () => {
+    this.props.history.replace("/login")
+  }
   render(){
     return(
       <div>
@@ -28,12 +44,20 @@ class Register extends Component{
           <WhiteSpace/>
           <List.Item>
             <span>用户类型: </span>&nbsp;&nbsp;
-            <Radio>大神</Radio>&nbsp;&nbsp;&nbsp;&nbsp;
-            <Radio>老板</Radio>
+            <Radio onClick={() => {this.handlerChange("type","dashen")}}
+                   checked={this.state.type === "dashen"}
+            >
+              大神
+            </Radio>&nbsp;&nbsp;&nbsp;&nbsp;
+            <Radio onClick={() => {this.handlerChange("type","laoban")}}
+                   checked={this.state.type === "laoban"}
+            >
+              老板
+            </Radio>
           </List.Item>
           <WhiteSpace/>
           <Button type="primary">注册</Button>
-          <Button>已有账号</Button>
+          <Button onClick={this.toLogin}>已有账号</Button>
         </List>
         </WingBlank>
       </div>
